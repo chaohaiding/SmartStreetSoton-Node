2fe54e36f280e93e1b867cf380c25149e4906357
Chaohai <chaohaiding@gmail.com>
2014-03-07 08:59:52 +0000

    changes

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,3 @@
-/*var edge = require('edge');
-
-var retrieve = edge.func('py','./python/retrieve.py');
-
-hello(54.7833333333,-127.177222222, function (error, result) {
-    if (error) throw error;
-    console.log(result);
-});
-var hello = edge.func('py', 'hello.py');
-
-hello('Node.js', function (error, result) {
-    if (error) throw error;
-    console.log(result);
-});*/
 
 module.exports = function (app) {
 // home route
@@ -27,6 +13,8 @@ app.get('/accidentmap',vehicles.index);
 app.get('/cyclespot',function(req, res){
   res.render('safemap', { title: 'SafeMap-SmartStreet',pageName:'Demon' });
 });
+app.get('/cyclespot',vehicles.cyclesafe);
+
 app.get('/contact', function(req, res){
   res.render('contact', { title: 'Contact-SmartStreet',pageName:'Contact' });
 });
